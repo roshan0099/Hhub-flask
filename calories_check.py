0b00fea5744ed9da2828a6423c7d4e28ba54f6af
Roshan <67196874+roshan0099@users.noreply.github.com>
2021-01-29 19:54:28 +0530

    Update calories_check.py

--- calories_check.py
+++ calories_check.py
@@ -4,7 +4,7 @@ import requests
 def req(queries) :
 	api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
 	# query ='3lb carrots and a chicken sandwich'
-	response = requests.get(api_url + queries, headers={'X-Api-Key': 'uoub/aG1tbr8CRCGXj3RIA==gH2MUnFR5ZPw0M6j'})
+	response = requests.get(api_url + queries, headers={'X-Api-Key': ''})
 	if response.status_code == requests.codes.ok:
 	    return(response.text)
 	else:
