aa12b1537654d3c1373b1a29f0a25a19c35fb795
roshan0099 <roshan09ka@gmail.com>
2021-01-29 10:16:46 +0530

    changed all over

--- app.py
+++ app.py
@@ -13,35 +13,35 @@ import os
 app = Flask(__name__)
 
 
-def covid_prediction(image_test):
-    model = load_model("model.h5")
-    model.compile(optimizer="adam", loss="bianry_crossentropy",
-                  metrics=["accuracy"])
-    image = cv2.imread(image_test)
-    image = cv2.resize(image, (64, 64))
-    image = np.reshape(image, [1, 64, 64, 3])
+# def covid_prediction(image_test):
+#     model = load_model("model.h5")
+#     model.compile(optimizer="adam", loss="bianry_crossentropy",
+#                   metrics=["accuracy"])
+#     image = cv2.imread(image_test)
+#     image = cv2.resize(image, (64, 64))
+#     image = np.reshape(image, [1, 64, 64, 3])
 
-    all_classes = model.predict_classes(image)
-    label = ["POSITIVE", "NEGATIVE"]
-    return label[all_classes[0][0]]
-
-
-@app.route("/covid")
-def covid():
-    return render_template("covid-19-index.html")
+#     all_classes = model.predict_classes(image)
+#     label = ["POSITIVE", "NEGATIVE"]
+#     return label[all_classes[0][0]]
 
 
-@app.route("/covid/upload-image", methods=['GET', 'POST'])
-def image_upload():
-    file = request.files['image']
-    if request.method == "POST":
-        if file:
-            file.save(file.filename)
-            label = covid_prediction(file.filename)
-            return render_template("covid-19-result.html", name=label)
+# @app.route("/covid")
+# def covid():
+#     return render_template("covid-19-index.html")
 
-        else:
-            return redirect('/covid')
+
+# @app.route("/covid/upload-image", methods=['GET', 'POST'])
+# def image_upload():
+#     file = request.files['image']
+#     if request.method == "POST":
+#         if file:
+#             file.save(file.filename)
+#             label = covid_prediction(file.filename)
+#             return render_template("covid-19-result.html", name=label)
+
+#         else:
+#             return redirect('/covid')
 
 def calccal(life,bmr):
     one = bmr*1.2
